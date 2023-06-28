1d1cf2f7803a199489d4ad0c3356fc3d4025b9cc
ewasawczynska <sawczynska.e@gmail.com>
2023-06-28 20:50:44 +0200

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+const pageGallery = document.querySelector('.gallery');
+
+const gallery = galleryItems
+  .map(
+    galleryItem =>
+      `<li class = "gallery__item"><a class = "gallery__link" href="${galleryItem.original}"><img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"/></a></li>`
+  )
+  .join('');
+
+pageGallery.insertAdjacentHTML('beforeend', gallery);
+
+pageGallery.style.listStyle = 'none';
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
