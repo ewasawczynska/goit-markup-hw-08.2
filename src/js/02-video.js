3a7ca594caec04de2df89ba0b338ee98462b8425
ewasawczynska <sawczynska.e@gmail.com>
2023-06-28 20:57:03 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(({ duration, percent, seconds }) => {
+    localStorage.setItem('videoplayer-current-time', `${seconds}`);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
